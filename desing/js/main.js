0963a19a08380b1586efa7747c894d5de21581f3
Yosvany Pedroso Maden <ypmaden@gmail.com>
2015-02-19 10:44:02 -0500

    page 404

--- desing/js/main.js
+++ desing/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+
     // Métodos para menejo de Cookie
     function setCookie(key, value) {
         var expires = new Date();
@@ -12,9 +13,9 @@ $(document).ready(function() {
     }
 
     // Click sobre elemento padre del menu
-    $('div.toggle').click(function(e) {
+    $('div.toggle').click(function (e) {
         e.preventDefault();
-        $(this).find('i').toggleClass('fa-chevron-right fa-chevron-down');
+        $(this).find('i') .toggleClass('fa-chevron-right fa-chevron-down');
 
         toggle = $(this).attr('data-toggle')
         if (getCookie(toggle) == 1) {
@@ -26,78 +27,135 @@ $(document).ready(function() {
 
     $('[id^=detail-]').hide();
 
-    $('.toggle').click(function() {
+    $('.toggle').click(function () {
         $input = $(this);
         $target = $('#' + $input.attr('data-toggle'));
         $target.slideToggle();
     });
 
     // Se comprueba que elementos del menú estaban abiertos
-    $('.list-group-item').each(function(index) {
+    $('.list-group-item').each(function (index) {
         id = $(this).attr('id');
         if (getCookie(id) == 1) {
             $(this).css('display', 'block');
             $('#dropdown-' + id).find('i').toggleClass('fa-chevron-right fa-chevron-down');
         }
     });
-    
-    // Para manejar el ancho del contenido    
-    $.contenWidth = function() {
+
+    //Para manejar el ancho del contenido    
+    $.contenWidth = function () {
         if ($(window).width() >= 768) {
-            $('.tdContentPlaceHolder').width($(window).width() - 280);
+            $('.tdContentPlaceHolder').width($(window).width() - $('#sidebar-wrapper').width()-10);
         } else {
             $('.tdContentPlaceHolder').width($(window).width());
         }
-    }      
-    
-    //para manejar el alto del contenido
-    $.contenHeight = function() {
-        $('.ContentContainer').height($(window).height() - 63);           
     }
-    
+
+    //para manejar el alto del contenido
+    $.contenHeight = function () {
+        $('.ContentContainer .tdContentPlaceHolder').height($(window).height() - 63);
+    }
+
     $.contenWidth();
     $.contenHeight();
-    $(window).resize(function() {
+    $(window).resize(function () {
         $.contenWidth();
         $.contenHeight();
-    });  
-        
-    $('.mainMenu a').each(function(index){
-        baseUrl = window.location.pathname.split('/'); 
-        obj = baseUrl[baseUrl.length-1]; 
-		
-		search = window.location.search;
-		if (search != ""){
-		  obj = obj+search;
-		}		
-		
-        if ($(this).attr('href') == obj){
-             $(this).addClass('activo');
-        }else{
+    });
+	
+    	var bandera = false;
+        var baseUrl = window.location.pathname.split('/'),
+            obj = baseUrl[baseUrl.length - 1],
+            search = window.location.search;
+        if (search != "") {
+            obj = obj + search;
+        }
+    $('.mainMenu a').each(function (index) {
+        if ($(this).attr('href')== obj) {
+            //Se garantiza que se posicione en el lugar donde seleccione
+            $('.sidebar-container').animate({scrollTop: $(this).position().top-50}, 'slow');
+            $(this).addClass('activo');
+            /**/
+            $.cookie('ultimoactivo', $(this).attr('href'));
+            bandera = true;
+        } else {
             $(this).removeClass('activo');
         }
-    });    
-    
+    });
+    if (!bandera) {
+        var ultimo = $.cookie('ultimoactivo');
+        $(".mainMenu li").find('a').each(function () {
+            var href2 = $(this).attr('href');
+            if (href2 == ultimo) {
+                $(this).addClass('activo');
+            }
+        });
+    }
+
     // Cambio de Container por ContainerFluid
-    $('div.Container').attr('class','container-fluid FormContainer');    
-    
+    $('div.Container').attr('class', 'container-fluid FormContainer');
+
     //Scroll en los Grids Largos
     $('#GRIDLARGEID').children('div').addClass('GridLarge');
+
+    //Para ocultar los nodos padres sin hijos
+
+    $('div.toggle').each(function () { 	//Se recorre por primera vez para eliminar padres sin hijos	
+        if ($(this).next().children().size() == 0) {
+            $(this).next().remove();
+            $(this).remove();
+        }
+    });
+    $('div.toggle').each(function () { 	//Se recorre para eliminar los padres nodos raiz
+        //sin hijos luego de haber pasado la primera vuelta
+        if ($(this).next().children().size() == 0) {
+            $(this).next().remove();
+            $(this).remove();
+        }
+    });
 	
-	//Para ocultar los nodos padres sin hijos
+	//Para que se muestre bien el sidebar menu
+	$(window).resize(function(){
+		var h1=$('body').height()-$("#sidebar-wrapper").offset().top;
+		$("#sidebar-wrapper").height(h1);
+		$("#sidebar-wrapper .sidebar-container").height(h1-56);
+	});
+    
+	setTimeout(function(){
+		$(window).resize();
 
-    $('div.toggle').each(function(){ 	//Se recorre por primera vez para eliminar padres sin hijos	
-		if ($(this).next().children().size() == 0){
-			 $(this).next().remove();
-			 $(this).remove();  
-		}
-    });
-	$('div.toggle').each(function(){ 	//Se recorre para eliminar los padres nodos raiz
-										//sin hijos luego de haber pasado la primera vuelta
-		if ($(this).next().children().size() == 0){
-			 $(this).next().remove();
-			 $(this).remove();  
-		}
-    });
+        $('.nivel2').each(function(){
+            var $this=$(this),
+                text=$this.text(),
+                i=$this.find('i');
+            $this.empty();
+            $this.append($("<span>"+text+"</span>"));
+            $this.append(i);
+            if(jQuery.fn.tooltip){
+                jQuery(this).tooltip(
+                {
+                    title: $(this).text(),
+                    placement: 'right',
+                    container: 'body'
+                });
+            }
+        });
 
+        if(jQuery.fn.tooltip){
+    		$(".sidebar-nav li a").each(function(){
+    			jQuery(this).tooltip(
+    			{
+    				title: $(this).text(),
+    				placement: 'right',
+    				container: 'body'
+    			});
+    		});
+        }
+	},100);
 });
+
+
+function ScrollTo() {
+   var div = $('#vLOGMENSAJES');
+   div.scrollTop(div.scrollTop() + div.innerHeight());
+}
