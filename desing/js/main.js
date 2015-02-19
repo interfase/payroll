fe71cfa005dfe67cddfb860e850ff1e36dd9b9e1
Yosvany Pedroso Maden <ypmaden@gmail.com>
2015-02-19 14:31:05 -0500

    fix error on !important

--- desing/js/main.js
+++ desing/js/main.js
@@ -1,157 +1,165 @@
+
 $(document).ready(function() {
-
-
-    // Métodos para menejo de Cookie
-    function setCookie(key, value) {
-        var expires = new Date();
-        expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
-        document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
-    }
-    function getCookie(key) {
-        var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
-        return keyValue ? keyValue[2] : null;
-    }
-
-    // Click sobre elemento padre del menu
-    $('div.toggle').click(function (e) {
-        e.preventDefault();
-        $(this).find('i') .toggleClass('fa-chevron-right fa-chevron-down');
-
-        toggle = $(this).attr('data-toggle')
-        if (getCookie(toggle) == 1) {
-            setCookie(toggle, 0);
-        } else {
-            setCookie(toggle, 1);
+                                            
+    //patch para un doble ready de genexus con los prompts, 
+    //no idea por que hace esto el monstruo del icono rojo
+    console.log(window.alreadyLoaded?"Already loaded":"Not loaded yet");
+    if(!window.alreadyLoaded){
+        window.alreadyLoaded=true;
+        // Métodos para menejo de Cookie
+        function setCookie(key, value) {
+            var expires = new Date();
+            expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+            document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
         }
-    });
-
-    $('[id^=detail-]').hide();
-
-    $('.toggle').click(function () {
-        $input = $(this);
-        $target = $('#' + $input.attr('data-toggle'));
-        $target.slideToggle();
-    });
-
-    // Se comprueba que elementos del menú estaban abiertos
-    $('.list-group-item').each(function (index) {
-        id = $(this).attr('id');
-        if (getCookie(id) == 1) {
-            $(this).css('display', 'block');
-            $('#dropdown-' + id).find('i').toggleClass('fa-chevron-right fa-chevron-down');
+        function getCookie(key) {
+            var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+            return keyValue ? keyValue[2] : null;
         }
-    });
 
-    //Para manejar el ancho del contenido    
-    $.contenWidth = function () {
-        if ($(window).width() >= 768) {
-            $('.tdContentPlaceHolder').width($(window).width() - $('#sidebar-wrapper').width()-10);
-        } else {
-            $('.tdContentPlaceHolder').width($(window).width());
+        // Click sobre elemento padre del menu
+        $('div.toggle').click(function (e) {
+            e.preventDefault();
+            $(this).find('i') .toggleClass('fa-chevron-right fa-chevron-down');
+
+            toggle = $(this).attr('data-toggle')
+            if (getCookie(toggle) == 1) {
+                setCookie(toggle, 0);
+            } else {
+                setCookie(toggle, 1);
+            }
+            $('#'+toggle).slideToggle();
+        });
+
+        $('[id^=detail-]').hide();
+
+        // $('.toggle').click(function () {
+        //     $input = $(this);
+        //     $target = $('#' + $input.attr('data-toggle'));
+
+        //     $target.slideToggle();
+        // });
+
+        // Se comprueba que elementos del menú estaban abiertos
+        $('.list-group-item').each(function (index) {
+            id = $(this).attr('id');
+            if (getCookie(id) == 1) {
+                $(this).css('display', 'block');
+                $('#dropdown-' + id).find('i').toggleClass('fa-chevron-right fa-chevron-down');
+            }
+        });
+
+        //Para manejar el ancho del contenido    
+        $.contenWidth = function () {
+            if ($(window).width() >= 768) {
+                $('.tdContentPlaceHolder').width($(window).width() - $('#sidebar-wrapper').width()-10);
+            } else {
+                $('.tdContentPlaceHolder').width($(window).width());
+            }
         }
-    }
 
-    //para manejar el alto del contenido
-    $.contenHeight = function () {
-        $('.ContentContainer .tdContentPlaceHolder').height($(window).height() - 63);
-    }
+        //para manejar el alto del contenido
+        $.contenHeight = function () {
+            $('.ContentContainer .tdContentPlaceHolder').height($(window).height() - 63);
+        }
 
-    $.contenWidth();
-    $.contenHeight();
-    $(window).resize(function () {
         $.contenWidth();
         $.contenHeight();
-    });
-	
-    	var bandera = false;
-        var baseUrl = window.location.pathname.split('/'),
-            obj = baseUrl[baseUrl.length - 1],
-            search = window.location.search;
-        if (search != "") {
-            obj = obj + search;
-        }
-    $('.mainMenu a').each(function (index) {
-        if ($(this).attr('href')== obj) {
-            //Se garantiza que se posicione en el lugar donde seleccione
-            $('.sidebar-container').animate({scrollTop: $(this).position().top-50}, 'slow');
-            $(this).addClass('activo');
-            /**/
-            $.cookie('ultimoactivo', $(this).attr('href'));
-            bandera = true;
-        } else {
-            $(this).removeClass('activo');
-        }
-    });
-    if (!bandera) {
-        var ultimo = $.cookie('ultimoactivo');
-        $(".mainMenu li").find('a').each(function () {
-            var href2 = $(this).attr('href');
-            if (href2 == ultimo) {
+        $(window).resize(function () {
+            $.contenWidth();
+            $.contenHeight();
+        });
+        
+            var bandera = false;
+            var baseUrl = window.location.pathname.split('/'),
+                obj = baseUrl[baseUrl.length - 1],
+                search = window.location.search;
+            if (search != "") {
+                obj = obj + search;
+            }
+        $('.mainMenu a').each(function (index) {
+            if ($(this).attr('href')== obj) {
+                //Se garantiza que se posicione en el lugar donde seleccione
+                $('.sidebar-container').animate({scrollTop: $(this).position().top-50}, 'slow');
                 $(this).addClass('activo');
+                /**/
+                $.cookie('ultimoactivo', $(this).attr('href'));
+                bandera = true;
+            } else {
+                $(this).removeClass('activo');
             }
         });
-    }
-
-    // Cambio de Container por ContainerFluid
-    $('div.Container').attr('class', 'container-fluid FormContainer');
-
-    //Scroll en los Grids Largos
-    $('#GRIDLARGEID').children('div').addClass('GridLarge');
-
-    //Para ocultar los nodos padres sin hijos
-
-    $('div.toggle').each(function () { 	//Se recorre por primera vez para eliminar padres sin hijos	
-        if ($(this).next().children().size() == 0) {
-            $(this).next().remove();
-            $(this).remove();
+        if (!bandera) {
+            var ultimo = $.cookie('ultimoactivo');
+            $(".mainMenu li").find('a').each(function () {
+                var href2 = $(this).attr('href');
+                if (href2 == ultimo) {
+                    $(this).addClass('activo');
+                }
+            });
         }
-    });
-    $('div.toggle').each(function () { 	//Se recorre para eliminar los padres nodos raiz
-        //sin hijos luego de haber pasado la primera vuelta
-        if ($(this).next().children().size() == 0) {
-            $(this).next().remove();
-            $(this).remove();
-        }
-    });
-	
-	//Para que se muestre bien el sidebar menu
-	$(window).resize(function(){
-		var h1=$('body').height()-$("#sidebar-wrapper").offset().top;
-		$("#sidebar-wrapper").height(h1);
-		$("#sidebar-wrapper .sidebar-container").height(h1-56);
-	});
-    
-	setTimeout(function(){
-		$(window).resize();
 
-        $('.nivel2').each(function(){
-            var $this=$(this),
-                text=$this.text(),
-                i=$this.find('i');
-            $this.empty();
-            $this.append($("<span>"+text+"</span>"));
-            $this.append(i);
+        // Cambio de Container por ContainerFluid
+        $('div.Container').attr('class', 'container-fluid FormContainer');
+
+        //Scroll en los Grids Largos
+        $('#GRIDLARGEID').children('div').addClass('GridLarge');
+
+        //Para ocultar los nodos padres sin hijos
+
+        $('div.toggle').each(function () {  //Se recorre por primera vez para eliminar padres sin hijos 
+            if ($(this).next().children().size() == 0) {
+                $(this).next().remove();
+                $(this).remove();
+            }
+        });
+        $('div.toggle').each(function () {  //Se recorre para eliminar los padres nodos raiz
+            //sin hijos luego de haber pasado la primera vuelta
+            if ($(this).next().children().size() == 0) {
+                $(this).next().remove();
+                $(this).remove();
+            }
+        });
+        
+        //Para que se muestre bien el sidebar menu
+        $(window).resize(function(){
+            var h1=$('body').height()-$("#sidebar-wrapper").offset().top;
+            $("#sidebar-wrapper").height(h1);
+            $("#sidebar-wrapper .sidebar-container").height(h1-56);
+        });
+        
+        setTimeout(function(){
+            $(window).resize();
+
+            $('.nivel2').each(function(){
+                var $this=$(this),
+                    text=$this.text(),
+                    i=$this.find('i');
+                $this.empty();
+                $this.append($("<span>"+text+"</span>"));
+                $this.append(i);
+                if(jQuery.fn.tooltip){
+                    jQuery(this).tooltip(
+                    {
+                        title: $(this).text(),
+                        placement: 'right',
+                        container: 'body'
+                    });
+                }
+            });
+
             if(jQuery.fn.tooltip){
-                jQuery(this).tooltip(
-                {
-                    title: $(this).text(),
-                    placement: 'right',
-                    container: 'body'
+                $(".sidebar-nav li a").each(function(){
+                    jQuery(this).tooltip(
+                    {
+                        title: $(this).text(),
+                        placement: 'right',
+                        container: 'body'
+                    });
                 });
             }
-        });
-
-        if(jQuery.fn.tooltip){
-    		$(".sidebar-nav li a").each(function(){
-    			jQuery(this).tooltip(
-    			{
-    				title: $(this).text(),
-    				placement: 'right',
-    				container: 'body'
-    			});
-    		});
-        }
-	},100);
+        },500);
+    }                              
 });
 
 
